237c607223c57e6e22867df4f33bdb91eb88acc2
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-09-30 21:03:14 -0700

    reservation table

---
+++ data/migrations/20190930205019_reservation.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema
+    .createTable("reservations", reservation =>{ 
+        reservation.increments();
+        reservation
+            .string("firstname", 32)
+            .notNullable();
+        reservation
+            .string("lastname", 32)
+            .notNullable();
+        reservation
+            .string("phonenumber")
+            .notNullable();
+        reservation
+            .integer("partysize")
+            .notNullable();
+        reservation.integer("user_id")
+            .unsigned()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema
+    .dropTableIfExists("reservations")
+};

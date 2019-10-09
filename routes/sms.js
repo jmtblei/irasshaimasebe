f7db0ee35eb4475cf265695b2a89dfd7f59662ed
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-10-08 23:43:50 -0700

    sms requests taking in dynamic sendto numbers and partysize

--- routes/sms.js
+++ routes/sms.js
@@ -5,7 +5,10 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+//sends sms to customer alerting them a table is ready
 router.post("/ready", restricted, (req, res) => {
+    const size = req.body.size;
+    const to = req.body.to;
     client.messages
         .create({
             body: `We are ready to accommodate your reservation for ${size}. Please come to the front with all members to be seated!`,
@@ -18,7 +21,9 @@ router.post("/ready", restricted, (req, res) => {
         .catch(err => res.send(err))
 })
 
+//sends sms to customer alerting them no more reservations available at this time
 router.post("/sorry", restricted, (req, res) => {
+    const to = req.body.to;
     client.messages
         .create({
             body: 'Unfortunately, we are unable to accommodate any reservations at this time. Sorry for the inconvenience.',
